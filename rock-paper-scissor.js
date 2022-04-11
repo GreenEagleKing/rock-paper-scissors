c886e83575c6bb1498534b3242457803b002ffb8
Ben King <benking1293@gmail.com>
2022-04-11 16:11:42 +0100

    PlayRound function added, game works correctly. Fixed playerchoice function by making it a variable.

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,19 +1,45 @@
+// Returning user input from html dropdown
+
+var playerChoice = function () {
+    const selectedValue = document.getElementById("handChoice").value;
+    return (selectedValue);
+}
+
+let playerSelection = playerChoice;
+
 // computerPlay function - computer randomly selects rock,paper or scissors and outputs
 
 function computerPlay() {
-    let randomSelect = ["Rock", "Paper", "Scissors"];
-    return randomSelect[Math.floor(Math.random() * 3 )];
-          
+    const shapes = ['Rock', 'Paper', 'Scissors'];
+    const random = Math.floor(Math.random() * shapes.length);
+    return (shapes[random]);
 }
 
-let computerResult = computerPlay();
+let computerSelection = computerPlay();
+console.log("Computer selected " + computerSelection);
 
-console.log(computerResult);
 
-// Returning user input from html dropdown
+// Play round of RPS using playerSelection and computerSelection
 
-function playerSelection() {
-    selectElement = document.querySelector('#handChoice');
-    output = selectElement.options[selectElement.selectedIndex].value;
-    console.log(output);
+
+function playRound (playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    playerSelection = playerChoice();
+    console.log("Play computer selected " + computerSelection);
+    
+        if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        alert("You win! Rock beats Scissors");
+    }   else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        alert("You win! Paper beats Rock");
+    }   else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        alert("You win! Scissors beat Paper");
+    }   else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        alert("You lose! Rock beats Scissors");
+    }   else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        alert("You lose! Paper beats Rock");
+    }   else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        alert("You lose! Scissors beats Paper");
+    }   else {
+        alert("Tie! Play again"); 
+    }
 }
